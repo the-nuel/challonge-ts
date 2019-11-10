6af25aeb926b8352242895c932fe83b6a8c35258
MetalMichael <michaelfiford@gmail.com>
2019-11-10 17:24:27 +0100

    Remove build files from git

--- build/base.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-/** Base class for challonge objects, holds at its core the api key */
-class ChallongeBase {
-    constructor(api_key) {
-        this.api_key = api_key;
-    }
-}
-exports.default = ChallongeBase;
--- build/interfaces/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var tournament_interface_1 = require("./tournament.interface");
-exports.TournamentInterfaces = tournament_interface_1.TournamentInterfaces;
